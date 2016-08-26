4e68057179b47f1b67e51ca340578ff054afc1d9
lakewik <wikjezioro@op.pl>
2016-08-26 13:51:06 +0200

    Create relay-daemon.py
    Main script

---
+++ relay-daemon.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+
+"""
+Python daemon from: 
+http://www.python.rk.edu.pl/w/p/proste-demony-uniksowe-w-pythonie/
+"""
+
+import sys, os, time, atexit
+from signal import SIGTERM
+import subprocess
+import urllib
+from subprocess import call
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class Daemon:
+	"""
+	A generic daemon class.
+	
+	Usage: subclass the Daemon class and override the run() method
+	"""
+	def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+		self.stdin = stdin
+		self.stdout = stdout
+		self.stderr = stderr
+		self.pidfile = pidfile
+	
+	def daemonize(self):
+		"""
+		do the UNIX double-fork magic, see Stevens' "Advanced 
+		Programming in the UNIX Environment" for details (ISBN 0201563177)
+		http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
+		"""
+		try: 
+			pid = os.fork() 
+			if pid > 0:
+				# exit first parent
+				sys.exit(0) 
+		except OSError, e: 
+			sys.stderr.write("fork #1 failed: %d (%s)" % (e.errno, e.strerror))
+			sys.exit(1)
+	
+		# decouple from parent environment
+		os.chdir("/") 
+		os.setsid() 
+		os.umask(0) 
+	
+		# do second fork
+		try: 
+			pid = os.fork() 
+			if pid > 0:
+				# exit from second parent
+				sys.exit(0) 
+		except OSError, e: 
+			sys.stderr.write("fork #2 failed: %d (%s)" % (e.errno, e.strerror))
+			sys.exit(1) 
+	
+		# redirect standard file descriptors
+		sys.stdout.flush()
+		sys.stderr.flush()
+		si = file(self.stdin, 'r')
+		so = file(self.stdout, 'a+')
+		se = file(self.stderr, 'a+', 0)
+		os.dup2(si.fileno(), sys.stdin.fileno())
+		os.dup2(so.fileno(), sys.stdout.fileno())
+		os.dup2(se.fileno(), sys.stderr.fileno())
+	
+		# write pidfile
+		atexit.register(self.delpid)
+		pid = str(os.getpid())
+		file(self.pidfile,'w+').write("%s" % pid)
+	
+	def delpid(self):
+		os.remove(self.pidfile)
+
+	def start(self):
+		"""
+		Start the daemon
+		"""
+		# Check for a pidfile to see if the daemon already runs
+		try:
+			pf = file(self.pidfile,'r')
+			pid = int(pf.read().strip())
+			pf.close()
+		except IOError:
+			pid = None
+	
+		if pid:
+			message = "pidfile %s already exist. Daemon already running?"
+			sys.stderr.write(message % self.pidfile)
+			sys.exit(1)
+		
+		# Start the daemon
+		self.daemonize()
+		self.run()
+
+	def stop(self):
+		"""
+		Stop the daemon
+		"""
+		# Get the pid from the pidfile
+		try:
+			pf = file(self.pidfile,'r')
+			pid = int(pf.read().strip())
+			pf.close()
+		except IOError:
+			pid = None
+	
+		if not pid:
+			message = "pidfile %s does not exist. Daemon not running?"
+			print bcolors.WARNING + "WARNING!" + bcolors.ENDC
+			sys.stderr.write(message % self.pidfile)
+			return # not an error in a restart
+
+		# Try killing the daemon process	
+		try:
+			while 1:
+				os.kill(pid, SIGTERM)
+				time.sleep(0.1)
+		except OSError, err:
+			err = str(err)
+			if err.find("No such process") > 0:
+				if os.path.exists(self.pidfile):
+					os.remove(self.pidfile)
+			else:
+				print str(err)
+				sys.exit(1)
+
+	def restart(self):
+		"""
+		Restart the daemon
+		"""
+		self.stop()
+		self.start()
+
+	def run(self):
+		"""
+		You should override this method when you subclass Daemon. It will be called after the process has been
+		daemonized by start() or restart().
+		"""
+
+class MyDaemon(Daemon):
+	def run(self):
+		while True:
+			import os
+			import urllib2, urlparse, gzip
+			from StringIO import StringIO
+			sock = urllib.urlopen("http://s1.thekrzos.eu:1338/4?token=tRBxqYCk&uptime=tu")
+			htmlSource = sock.read()
+			sock.close()
+			if htmlSource == "1":
+				os.system("gpio -g write 17 1")
+			else:
+				os.system("gpio -g write 17 0")
+			time.sleep(0.1)
+			
+
+if __name__ == "__main__":
+	daemon = MyDaemon('/tmp/daemon-example.pid')
+	if len(sys.argv) == 2:
+		if 'start' == sys.argv[1]:
+			print "Starting weatcher logger daemon"
+			print "["+bcolors.OKGREEN + "SUCCESS" +"]"+ bcolors.ENDC
+			daemon.start()
+		elif 'stop' == sys.argv[1]:
+			print "Stopping weatcher logger daemon"
+			print "["+bcolors.OKGREEN + "SUCCESS" +"]"+ bcolors.ENDC
+			daemon.stop()
+		elif 'restart' == sys.argv[1]:
+			print "Stopping weatcher logger daemon"
+			print "Starting weatcher logger daemon"
+			logfile = open('/var/log/lakewik/lakewik.log', 'a')
+			logfile.write('Restarting lakewik Logger Daemon')
+			logfile.close()
+			daemon.restart()
+			print "["+bcolors.OKGREEN + "SUCCESS" +"]"+ bcolors.ENDC
+		else:
+			print "["+bcolors.FAIL + "FAIL" + bcolors.ENDC+"]"+"Unknown command"
+			sys.exit(2)
+		sys.exit(0)
+	else:
+		print "usage: %s start|stop|restart" % sys.argv[0]
+		sys.exit(2)
